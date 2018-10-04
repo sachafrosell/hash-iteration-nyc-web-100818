04350ec49da6946ae6bb77e6d6e51df6c585eda1
Alexander Frosell <sachafrosell@gmail.com>
2018-10-04 18:35:54 +0000

    automatically backed up by learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -1,12 +1,15 @@
-# passengers = {
-# suite_a: "Amanda Presley", 
-# suite_b: "Seymour Hoffman", 
-# suite_c: "Alfred Tennyson", 
-# suite_d: "Charlie Chaplin", 
-# suite_e: "Crumpet the Elf"
-# }
+ passengers = {
+ suite_a: "Amanda Presley", 
+ suite_b: "Seymour Hoffman", 
+ suite_c: "Alfred Tennyson", 
+ suite_d: "Charlie Chaplin", 
+ suite_e: "Crumpet the Elf"
+ }
 
 def select_winner(passengers)
   winner = ""
-  passen
+  passengers.each do |suite, name|
+    if suite == :suita_a && name.start_with?("A")
+      winner = name
+    end
 end
